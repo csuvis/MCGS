5568257ccae0dc6ea7f69fbc78d0f1f7c44b636e
csuvis <zhaoying511@126.com>
2020-08-24 13:37:01 +0800

    update usage_example.py

--- usage_example.py
+++ usage_example.py
@@ -1,3 +1,5 @@
+import os
+
 from MCGS import MCGS
 from utils.load_graph import loadGraph
 from utils.save_file import saveAsSVG
@@ -8,6 +10,8 @@ def main():
     dataset = 'As-733'
     G = loadGraph(dataset)  # load original graph
     Gs = MCGS_model.run_sampling(G, 0.3, alpha=1, beta=4, loss_weight=[1, 0, 0])  # run the samping algorithm
+    if not os.path.exists('output'):
+    	os.makedirs('output') 
     saveAsSVG(dataset, Gs)  # save the sampling graph data as SVG type file
     print('Sampling completed successfully!')
     print('Sampling Graph nodes: {}'.format(len(Gs.nodes())))
